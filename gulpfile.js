2fae078f7131a14ddf14b80a67f4d838b67d81a0
Giacomo Bortolotti <giacomo.bortolotti@ynap.com>
2021-02-14 15:14:12 +0100

    Update Gulp Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,51 @@
-var gulp = require("gulp"),
-  path = require("path"),
-  paths = require("./task/gulp/paths"),
+const gulp = require("gulp"),
+  paths = require("./tasks/gulp/paths"),
   sass = require("gulp-sass"),
   rename = require("gulp-rename"),
   cssmin = require("gulp-cssnano"),
   prefix = require("gulp-autoprefixer"),
   sourcemaps = require("gulp-sourcemaps"),
-  sassPath = path.join(paths.sass_path, "**/*.scss");
+  browsersync = require("browser-sync").create(),
+  sassPath = "./dev/sass/**/*.scss";
 
-sass.compiler = require("node-sass");
+sass.compiler = require("node-sass")
 
-gulp.task("GB::update::style", () => {
+function browserSync(done) {
+  browsersync.init({
+    server: paths.proj_path+"/",
+    files: [
+      "./"+paths.css_prod_path+"/*.css",
+    ],
+    port: 3000,
+    open: false,
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function updateStyle() {
   return gulp
     .src(sassPath)
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(paths.css_dev_path))
+    .pipe(prefix())
     .pipe(cssmin())
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(paths.css_prod_path));
-});
+    .pipe(gulp.dest(paths.css_prod_path))
+    .pipe(browsersync.stream());
+}
 
-gulp.task("GB::sass::watch", () => {
+function watchFiles() {
   gulp
-    .watch(sassPath, ["GB::update::style"])
-    .on("error", (error) => {
-      console.log(error);
-    })
-    .on("change", (event) => {
-      console.log("\n\tFile " + event.path + " was " + event.type + "\n");
-    });
-});
+    .watch(sassPath, updateStyle);
+}
+
+const watch = gulp.parallel(watchFiles, browserSync);
+
+exports.watch = watch;
